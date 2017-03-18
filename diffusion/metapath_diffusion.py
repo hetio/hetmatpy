2dcb6c4e778ac6f52b37de77392f48b3dbdb0599
kkloste <kyle.kloster@gmail.com>
2017-03-18 00:57:56 -0400

    Getting test function ready for diffusion codes
    Former-commit: https://github.com/greenelab/hetmech/commit/bdc499ef8c94b702fb25fb5e30e626e04d31ec8b

--- diffusion/metapath_diffusion.py
+++ diffusion/metapath_diffusion.py
@@ -1,7 +1,10 @@
 import hetio.hetnet
 import collections
 import numpy
+import pytest
 
+   
+    
 def dual_normalize(matrix, row_damping=0, column_damping=0):
     """
     Row and column normalize a 2d numpy array
@@ -92,3 +95,44 @@ def diffuse_along_metapath(graph, metapath, source_node_weights, column_damping=
     target_node_to_position = get_node_to_position(graph, target_metanode)
     node_to_score = collections.OrderedDict(zip(target_node_to_position, node_scores))
     return node_to_score
+
+
+
+def test_dual_normalize():
+'''
+    First attempt at a pytest test.
+    The test itself runs properly, and checks
+    that dual_normalize is doing what it is supposed to.
+'''
+    toy_matrix = numpy.array([ 
+        [1, 1, 1],
+        [1, 1, 0],
+        [1, 0, 0]
+        ])
+    toy_matrix = toy_matrix.astype('float64')
+    toy_copy = toy_matrix.copy()
+    test1 = dual_normalize( toy_copy, 0.0, 0.0 )
+    test1 = test1.astype('float64')
+    assert(  numpy.sum(numpy.absolute(toy_matrix-test1)) == 0 )
+
+    test_exponent = 0.5
+    toy_copy = toy_matrix.copy()
+    test2 = dual_normalize( toy_copy, test_exponent, 0.0 )
+    test2.astype('float64')
+    true_matrix2 = numpy.array([ 
+        [1/3**test_exponent, 1/3**test_exponent, 1/3**test_exponent],
+        [1/2**test_exponent, 1/2**test_exponent, 0],
+        [1, 0, 0]
+        ])
+    assert(  numpy.sum(numpy.absolute(true_matrix2-test2)) < 10*numpy.finfo(float).eps )
+
+    test_exponent = 0.3
+    toy_copy = toy_matrix.copy()
+    test3 = dual_normalize( toy_copy, 0, test_exponent )
+    true_matrix3 = numpy.array([ 
+        [1/3**test_exponent, 1/3**test_exponent, 1/3**test_exponent],
+        [1/2**test_exponent, 1/2**test_exponent, 0],
+        [1, 0, 0]
+        ])
+    true_matrix3 = numpy.transpose(true_matrix3)
+    assert(  numpy.sum(numpy.absolute(true_matrix3-test3)) < 10*numpy.finfo(float).eps )
