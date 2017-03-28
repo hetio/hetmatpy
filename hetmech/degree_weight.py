e9c548b511b6f6a563209b696f657666190791b7
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-03-28 14:39:31 -0400

    Implement matrix DWWC
    Split into diffusion, matrix & degree_weight modules
    
    
    Former-commit: https://github.com/greenelab/hetmech/commit/ef573fd2957df2b7ce6769aca82538ac82ddf928

---
+++ hetmech/degree_weight.py
@@ -0,0 +1,57 @@
+import numpy
+
+from .matrix import normalize, metaedge_to_adjacency_matrix
+
+
+def dwwc_step(
+        matrix, row_damping=0, column_damping=0, copy=True):
+    """
+    Return the degree-weighted adjacency matrix produced by the input matrix
+    with the specified row and column normalization exponents.
+
+    Parameters
+    ==========
+    matrix : numpy.ndarray
+        adjacency matrix for a given metaedge, where the source nodes are
+        columns and the target nodes are rows
+    row_damping : int or float
+        exponent to use in scaling each node's row by its in-degree
+    column_damping : int or float
+        exponent to use in scaling each node's column by its column-sum
+    copy : bool
+        `True` gaurantees matrix will not be modified in place. `False`
+        modifies in-place if and only if matrix.dtype == numpy.float64.
+        Users are recommended not to rely on in-place conversion, but instead
+        use `False` when in-place modification is acceptable and efficiency
+        is desired.
+
+    Returns
+    =======
+    numpy.ndarray
+        Normalized matrix with dtype.float64.
+    """
+    # returns a newly allocated numpy.ndarray
+    matrix = numpy.array(matrix, numpy.float64, copy=copy)
+    assert matrix.ndim == 2
+
+    row_sums = matrix.sum(axis=1)
+    column_sums = matrix.sum(axis=0)
+    matrix = normalize(matrix, row_sums, 'rows', row_damping)
+    matrix = normalize(matrix, column_sums, 'columns', column_damping)
+
+    return matrix
+
+
+def dwwc(graph, metapath, damping=0.5):
+    """
+    Compute the degree-weighted walk count (DWWC).
+    """
+    dwwc_matrix = None
+    for metaedge in metapath:
+        adj_mat = metaedge_to_adjacency_matrix(graph, metaedge)
+        adj_mat = dwwc_step(adj_mat, damping, damping)
+        if dwwc_matrix is None:
+            dwwc_matrix = adj_mat
+        else:
+            dwwc_matrix = adj_mat @ dwwc_matrix
+    return dwwc_matrix
--- hetmech/diffusion.py
+++ hetmech/diffusion.py
@@ -1,7 +1,10 @@
 from collections import OrderedDict
 
 import numpy
-import hetio.hetnet
+
+from .matrix import (normalize,
+                     get_node_to_position,
+                     metaedge_to_adjacency_matrix)
 
 
 def diffusion_step(
@@ -46,62 +49,6 @@ def diffusion_step(
         matrix = normalize(matrix, column_sums, 'columns', column_damping)
 
     return matrix
-
-
-def normalize(matrix, vector, axis, damping_exponent):
-    """
-    Normalize a 2D numpy.ndarray in place.
-
-    Parameters
-    ==========
-    matrix : numpy.ndarray
-    vector : numpy.ndarray
-        Vector used for row or column normalization of matrix.
-    axis : str
-        'rows' or 'columns' for which axis to normalize
-    """
-    assert matrix.ndim == 2
-    assert vector.ndim == 1
-    if damping_exponent == 0:
-        return matrix
-    with numpy.errstate(divide='ignore'):
-        vector **= -damping_exponent
-    vector[numpy.isinf(vector)] = 0
-    shape = (len(vector), 1) if axis == 'rows' else (1, len(vector))
-    matrix *= vector.reshape(shape)
-    return matrix
-
-
-def get_node_to_position(graph, metanode):
-    """
-    Given a metanode, return a dictionary of node to position
-    """
-    if not isinstance(metanode, hetio.hetnet.MetaNode):
-        # metanode is a name
-        metanode = graph.metagraph.node_dict[metanode]
-    metanode_to_nodes = graph.get_metanode_to_nodes()
-    nodes = sorted(metanode_to_nodes[metanode])
-    node_to_position = OrderedDict((n, i) for i, n in enumerate(nodes))
-    return node_to_position
-
-
-def metaedge_to_adjacency_matrix(graph, metaedge, dtype=numpy.bool_):
-    """
-    Returns an adjacency matrix where source nodes are columns and target
-    nodes are rows.
-    """
-    if not isinstance(metaedge, hetio.hetnet.MetaEdge):
-        # metaedge is an abbreviation
-        metaedge = graph.metagraph.metapath_from_abbrev(metaedge)[0]
-    source_nodes = list(get_node_to_position(graph, metaedge.source))
-    target_node_to_position = get_node_to_position(graph, metaedge.target)
-    shape = len(target_node_to_position), len(source_nodes)
-    adjacency_matrix = numpy.zeros(shape, dtype=dtype)
-    for j, source_node in enumerate(source_nodes):
-        for edge in source_node.edges[metaedge]:
-            i = target_node_to_position[edge.target]
-            adjacency_matrix[i, j] = 1
-    return adjacency_matrix
 
 
 def diffusion(
