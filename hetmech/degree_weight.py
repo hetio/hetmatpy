bbf164ee44bafe63b2c2b3e61dadc24dde0414f2
Michael Zietz <michael.zietz@gmail.com>
2018-05-21 09:56:56 -0400

    degree_weight._dwpc_disjoint: recurse dwpc call for simplicity (greenelab/hetmech#102)
    Closes https://github.com/greenelab/hetmech/pull/102
    
    
    Former-commit: https://github.com/greenelab/hetmech/commit/032b5ecd90e337d28d5b75657b256bf83a7e53ed

--- hetmech/degree_weight.py
+++ hetmech/degree_weight.py
@@ -363,11 +363,9 @@ def _dwpc_disjoint(graph, metapath, damping=0.5, dense_threshold=0,
     col_names = None
     dwpc_matrix = None
     for segment in segments:
-        segment_category = categorize(segment)
-        dwpc_function = category_to_function(segment_category)
-        rows, cols, seg_matrix = dwpc_function(
-            graph, segment, damping=damping, dense_threshold=dense_threshold,
-            dtype=dtype)
+        rows, cols, seg_matrix, t = dwpc(graph, segment, damping=damping,
+                                         dense_threshold=dense_threshold,
+                                         dtype=dtype)
         if row_names is None:
             row_names = rows
         if segment is segments[-1]:
