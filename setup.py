011bb2313e92e7078b93b203c2078b4fec5de217
Greg Way <gregory.way@gmail.com>
2018-10-30 15:25:04 -0400

    setup.py: find_packages to fix hetmech.hetmat submodule import
    Merges https://github.com/greenelab/hetmech/pull/147
    Refs https://github.com/greenelab/hetmech/issues/145#issuecomment-434412918
    
    See https://stackoverflow.com/a/50120983
    
    Former-commit: https://github.com/greenelab/hetmech/commit/17b8b2bbfc6a88929e80292ed0c62a4813f073f2

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='hetmech',
       description='A search engine for hetnets',
       long_description='Matrix implementations of path-count-based measures',
       url='https://github.com/greenelab/hetmech',
       license='BSD 3-Clause License',
-      packages=['hetmech'],
+      packages=find_packages(),
       install_requires=[
           'hetio>=0.2.8',
           'numpy',
