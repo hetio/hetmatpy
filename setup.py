8eb66c9d8417044eed5c0f1abcbb98e77e770324
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2018-11-08 14:15:01 -0500

    Whitespace sytle of setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
-from setuptools import find_packages
+import setuptools
 
-setup(name='hetmech',
-      description='A search engine for hetnets',
-      long_description='Matrix implementations of path-count-based measures',
-      url='https://github.com/greenelab/hetmech',
-      license='BSD 3-Clause License',
-      packages=find_packages(),
-      install_requires=[
-          'hetio>=0.2.8',
-          'numpy',
-          'scipy',
-          'xarray',
-          ]
-      )
+setuptools.setup(
+    name='hetmech',
+    description='A search engine for hetnets',
+    long_description='Matrix implementations of path-count-based measures',
+    url='https://github.com/greenelab/hetmech',
+    license='BSD 3-Clause License',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'hetio>=0.2.8',
+        'numpy',
+        'scipy',
+        'xarray',
+    ],
+)
