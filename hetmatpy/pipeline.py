b4f82acbbbd66c90fc17da98e06588b3b10daecd
Ben Heil <ben.jer.heil@gmail.com>
2019-03-01 16:34:58 -0500

    Refactor & test hetmatpy.pipeline. Improve empirical p-values
    Merges https://github.com/hetio/hetmatpy/pull/7

--- hetmatpy/pipeline.py
+++ hetmatpy/pipeline.py
@@ -1,5 +1,6 @@
 import itertools
 
+import numpy
 import pandas
 import scipy.special
 import scipy.stats
@@ -7,6 +8,27 @@ import scipy.stats
 import hetmatpy.degree_group
 import hetmatpy.degree_weight
 import hetmatpy.hetmat
+
+FLOAT_ERROR_TOLERANCE = 1e-5
+
+
+def calculate_sd(sum_of_squares, unsquared_sum, number_nonzero):
+    """
+    Calculate the standard deviation and validate the incoming data
+    """
+    if number_nonzero == 1:
+        return None
+
+    squared_deviations = sum_of_squares - unsquared_sum ** 2 / number_nonzero
+
+    # If all the values in the row are the same we'll manually return zero,
+    # because not doing so can lead to some issues with float imprecision
+    # The true value of the squared deviation will always be >= zero,
+    # but float error may bring it below zero
+    if abs(squared_deviations) < FLOAT_ERROR_TOLERANCE:
+        return 0.0
+    else:
+        return (squared_deviations / (number_nonzero - 1)) ** 0.5
 
 
 def add_gamma_hurdle_to_dgp_df(dgp_df):
@@ -26,10 +48,65 @@ def add_gamma_hurdle_to_dgp_df(dgp_df):
         )
     # Compute gamma-hurdle parameters
     dgp_df['mean_nz'] = dgp_df['sum'] / dgp_df['nnz']
-    dgp_df['sd_nz'] = ((dgp_df['sum_of_squares'] - dgp_df['sum'] ** 2 / dgp_df['nnz']) / (dgp_df['nnz'] - 1)) ** 0.5
-    dgp_df['beta'] = dgp_df['mean_nz'] / dgp_df['sd_nz'] ** 2
+
+    dgp_df['sd_nz'] = dgp_df[['sum_of_squares', 'sum', 'nnz']].apply(lambda row: calculate_sd(*row), raw=True, axis=1)
+
+    dgp_df['beta'] = (dgp_df['mean_nz'] / dgp_df['sd_nz'] ** 2).replace(numpy.inf, numpy.nan)
     dgp_df['alpha'] = dgp_df['mean_nz'] * dgp_df['beta']
+
     return dgp_df
+
+
+def calculate_gamma_hurdle_p_value(row):
+    """
+    Use the gamma hurdle model to calculate the p_value for a metapath
+    """
+    return row['nnz'] / row['n'] * (scipy.special.gammaincc(row['alpha'], row['beta'] * row['dwpc']))
+
+
+def path_does_not_exist(row):
+    """
+    Check whether any paths exist between the source and target. We know there
+    isn't a path if the row has a zero path count, or has a zero dwpc if the path
+    count isn't present in the row
+    """
+    if 'path_count' in row:
+        return row['path_count'] == 0
+    return row['dwpc'] == 0
+
+
+def calculate_empirical_p_value(row):
+    """
+    Calculate p_value in cases where the gamma hurdle model won't work
+    """
+    if path_does_not_exist(row):
+        # No paths exist between the given source and target nodes
+        return 1.0
+    if row['nnz'] == 0:
+        # No nonzero DWPCs are found in the permuted network, but paths are
+        # observed in the true network
+        return 0.0
+    if row['sd_nz'] == 0:
+        # The DWPCs in the permuted network are identical
+        if row['dwpc'] <= row['mean_nz']:
+            # The DWPC you found in the true network is smaller than or equal
+            # to those in the permuted network
+            return row['nnz'] / row['n_dwpcs']
+
+        # The DWPC you found in the true network is larger than those in the
+        # permuted network
+        return 0.0
+    raise NotImplementedError
+
+
+def calculate_p_value(row):
+    """
+    Calculate the p_value for a given metapath
+    """
+    if row['nnz'] == 0 or path_does_not_exist(row) or row['sd_nz'] == 0:
+        return calculate_empirical_p_value(row)
+    else:
+        return calculate_gamma_hurdle_p_value(row)
 
 
 def combine_dwpc_dgp(graph, metapath, damping, ignore_zeros=False, max_p_value=1.0):
@@ -47,13 +124,7 @@ def combine_dwpc_dgp(graph, metapath, damping, ignore_zeros=False, max_p_value=1
         degrees = row['source_degree'], row['target_degree']
         dgp = degrees_to_dgp[degrees]
         row.update(dgp)
-        if row['path_count'] == 0:
-            row['p_value'] = 1.0
-        else:
-            row['p_value'] = None if row['sum'] == 0 else (
-                row['nnz'] / row['n'] *
-                (scipy.special.gammaincc(row['alpha'], row['beta'] * row['dwpc']))
-            )
+        row['p_value'] = calculate_p_value(row)
         if row['p_value'] is not None and row['p_value'] > max_p_value:
             continue
         for key in ['sum', 'sum_of_squares', 'beta', 'alpha']:
--- hetmatpy/tests/test_pipeline.py
+++ hetmatpy/tests/test_pipeline.py
@@ -1,5 +1,13 @@
+import numpy
+import pandas
+import pytest
+
 from hetmatpy.pipeline import (
     grouper,
+    calculate_sd,
+    calculate_p_value,
+    add_gamma_hurdle_to_dgp_df,
+    path_does_not_exist,
 )
 
 
@@ -46,3 +54,124 @@ def test_grouper_length_1():
         (2,),
         (3,),
     ]
+
+
+@pytest.mark.parametrize('sum_of_squares, unsquared_sum, number_nonzero, expected_output', [
+    # Sum of squares and unsquared sum are from a pair of the same number, so return zero
+    (32.0, 8.0, 2.0, 0.0),
+    # Sum of squares and unsquared sum are very close to the case above, so return zero
+    (32.0, 8.0 + 1e-6, 2.0, 0.0),
+    # Only one nonzero observation, so return zero
+    (5.0, 5.0, 1.0, None),
+    # Test that the standard deviation of 5, 4, and 3 is 1
+    (50.0, 12.0, 3.0, 1.0),
+])
+def test_calculate_sd(sum_of_squares, unsquared_sum, number_nonzero, expected_output):
+    assert calculate_sd(sum_of_squares, unsquared_sum, number_nonzero) == expected_output
+
+
+@pytest.mark.parametrize('row, expected_output', [
+    # Zero path count
+    ({'path_count': 0,
+      'sd_nz': 2.0,
+      'dwpc': 4.0,
+      'nnz': 1,
+      'n': 4,
+      'alpha': 1.0,
+      'beta': 2.0,
+      'sum': 1.0
+      }, 1.0),
+    # zero standard deviation with dwpc lower than mean
+    ({'path_count': 5,
+      'sd_nz': 0.0,
+      'dwpc': 2.0,
+      'mean_nz': 3.0,
+      'nnz': 3,
+      'n_dwpcs': 8,
+      'n': 4,
+      'alpha': 1.0,
+      'beta': 2.0,
+      'sum': 1.0
+      }, .375),
+    # zero standard deviation with dwpc higher than mean
+    ({'path_count': 5,
+      'sd_nz': 0.0,
+      'dwpc': 4.0,
+      'mean_nz': 3.0,
+      'nnz': 1,
+      'n': 4,
+      'alpha': 1.0,
+      'beta': 2.0,
+      'sum': 1.0
+      }, 0.0),
+    # Normal gamma hurdle case
+    ({'path_count': 5,
+      'sd_nz': 1.0,
+      'dwpc': 2.5,
+      'nnz': 1,
+      'n': 10,
+      'alpha': 1.0,
+      'beta': 1.0,
+      'sum': 1.0
+      }, .008208),
+    # number nonzero is itself zero
+    ({'path_count': 5,
+      'sd_nz': 0.0,
+      'dwpc': 2.0,
+      'nnz': 0,
+      'n': 4,
+      'alpha': 1.0,
+      'beta': 2.0,
+      'sum': 0.0
+      }, 0.0),
+])
+def test_calculate_p_value(row, expected_output):
+    assert calculate_p_value(row) == pytest.approx(expected_output, rel=1e-4)
+
+
+def test_add_gamma_hurdle():
+    df_dict = {'nnz': [1, 3, 3],
+               'sum': [4.0, 4.0, 3.0],
+               'sum_of_squares': [4.0, 6.0, 3.0 + 1e-15],
+               }
+    dgp_df = pandas.DataFrame(df_dict)
+    dgp_df = add_gamma_hurdle_to_dgp_df(dgp_df)
+
+    # Test nnz = 1
+    expected_mean_nz_0 = 4.0
+    assert expected_mean_nz_0 == dgp_df['mean_nz'][0]
+    assert numpy.isnan(dgp_df['sd_nz'][0])
+    assert numpy.isnan(dgp_df['beta'][0])
+    assert numpy.isnan(dgp_df['alpha'][0])
+
+    # Test a normal case
+    expected_mean_nz_1 = 4 / 3
+    expected_sd_nz_1 = ((2 / 3) / 2) ** .5
+    expected_beta_1 = expected_mean_nz_1 / expected_sd_nz_1 ** 2
+    expected_alpha_1 = expected_mean_nz_1 * expected_beta_1
+    assert expected_mean_nz_1 == dgp_df['mean_nz'][1]
+    assert expected_sd_nz_1 == pytest.approx(dgp_df['sd_nz'][1])
+    assert expected_beta_1 == pytest.approx(dgp_df['beta'][1])
+    assert expected_alpha_1 == pytest.approx(dgp_df['alpha'][1])
+
+    # Test squared deviations ~ 0
+    expected_mean_nz_2 = 1.0
+    expected_sd_nz_2 = 0.0
+    assert expected_mean_nz_2 == dgp_df['mean_nz'][2]
+    assert expected_sd_nz_2 == dgp_df['sd_nz'][2]
+    assert numpy.isnan(dgp_df['beta'][2])
+    assert numpy.isnan(dgp_df['alpha'][2])
+
+
+@pytest.mark.parametrize('row, expected_output', [
+    # Path count is zero
+    ({'path_count': 0.0}, True),
+    # Path count is nonzero
+    ({'path_count': 1.0}, False),
+    # No path count, dwpc is zero
+    ({'dwpc': 0.0}, True),
+    # No path count, dwpc is nonzero
+    ({'dwpc': .01}, False),
+])
+def test_path_does_not_exist(row, expected_output):
+    assert path_does_not_exist(row) == expected_output
