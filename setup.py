bc36aa9859c43a1a5fb22808cd6eb952ef9d497c
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-05-27 15:15:44 -0400

    expand setup.py and add __version__

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
+import pathlib
+import re
+
 import setuptools
+
+directory = pathlib.Path(__file__).parent.resolve()
+
+# version
+init_path = directory.joinpath("hetmatpy", "__init__.py")
+text = init_path.read_text(encoding="utf-8-sig")
+pattern = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]", re.MULTILINE)
+version = pattern.search(text).group(1)
+
+# long_description
+readme_path = directory.joinpath("README.md")
+long_description = readme_path.read_text(encoding="utf-8-sig")
 
 setuptools.setup(
     name='hetmatpy',
-    description='A search engine for hetnets',
-    long_description='Matrix implementations of path-count-based measures',
+    description='Matrix implementations for hetnets and path-count-based measures',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     url='https://github.com/hetio/hetmatpy',
+    project_urls={
+        "Source": "https://github.com/hetio/hetmatpy",
+        "Documentation": "https://hetio.github.io/hetmatpy",
+        "Tracker": "https://github.com/hetio/hetmatpy/issues",
+        "Homepage": "https://het.io/software/",
+        "Publication": "https://greenelab.github.io/connectivity-search-manuscript/",
+    },
     license='BSD-2-Clause Plus Patent License',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
