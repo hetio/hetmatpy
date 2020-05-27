643b5a9d292c9b9d25ad9aa4fb4917761c955e8c
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-05-27 13:29:48 -0400

    Switch license to BSD-2-Clause-Patent
    BSD-2-Clause-Patent is the top rated license at
    https://blueoakcouncil.org/list
    that is also currently OSI-approved.
    
    License text from
    https://opensource.org/licenses/BSDplusPatent
    https://spdx.org/licenses/BSD-2-Clause-Patent

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     description='A search engine for hetnets',
     long_description='Matrix implementations of path-count-based measures',
     url='https://github.com/hetio/hetmatpy',
-    license='BSD 3-Clause License',
+    license='BSD-2-Clause Plus Patent License',
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     install_requires=[
